164b161e94ced24a5da31906adaf19cf6b1a2239
Vivek Myers <vmyers@stanford.edu>
2019-07-01 14:26:34 -0400

    First Commit

---
+++ agents/base.py
@@ -0,0 +1,35 @@
+class BaseAgent:
+    def __init__(self, prior, length, batch):
+        self.seen = prior
+        self.batch = batch
+        self.encode_cache = {}
+        self.embed_cache = {}
+        self.proj = np.random.normal(size=[4, length * 4])
+        
+    def encode(self, seq):
+        if seq in self.encode_cache:
+            return self.encode_cache[seq]
+        assert seq[0] in '+-'
+        arr = np.zeros([len(seq), 4])
+        arr[0, :] = 1 if seq[0] == '-' else 0
+        arr[(np.arange(1, len(seq)), ['ATCG'.index(i) for i in seq[1:]])] = 1
+        self.encode_cache[seq] = arr
+        return arr
+    
+    def embed(self, seq):
+        if seq in self.embed_cache:
+            return self.embed_cache[seq]
+        arr = self.proj @ self.encode(seq).flatten()[:, np.newaxis]
+        self.embed_cache[seq] = arr
+        return arr
+    
+    def act(self, data):
+        return sample(data, self.batch)
+    
+    def observe(self, obs):
+        self.seen = {**self.seen, **obs}
+    
+    def predict(self, seqs):
+        return [self.seen[min(sample(self.seen.keys(), self.batch), 
+                    key=lambda x: np.linalg.norm(self.embed(x) - self.embed(seq).flatten()))] 
+                    for seq in seqs]
---
+++ env.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import pandas as pd
+from tqdm import tqdm_notebook as tqdm
+import seaborn as sns
+get_ipython().run_line_magic("matplotlib", "inline")
+from itertools import *
+from collections import *
+from random import *
+
+class GuideEnv:
+    def __init__(self, files=['hela.csv', 'hek293t.csv', 'hct116.csv', 'hl60.csv'], 
+                     batch=1000, validation=0.2, initial=0.0):
+        dfs = list(map(pd.read_csv, files))
+        data = [(strand + seq, score) for df in dfs
+            for _, strand, seq, score in 
+            df[['Strand', 'sgRNA', 'Normalized efficacy']].itertuples()]
+        self.len = len(data[0][0])
+        shuffle(data)
+        r = int(validation * len(data))
+        self.env = dict(data[r:])
+        self.val = tuple(np.array(x) for x in zip(*data[:r]))
+        self.prior = dict(sample(self.env.items(), int(initial * len(data))))
+        self.batch = batch
+        
+    def run(self, Agent):
+        agent = Agent(self.prior.copy(), self.len, self.batch)
+        data = self.env.copy()
+        results = []
+        pbar = tqdm(total=len(data) // self.batch * self.batch)
+        while len(data) > self.batch:
+            sampled = agent.act(list(data.keys()))
+            agent.observe({seq: data[seq] for seq in sampled})
+            for seq in sampled:
+                del data[seq]
+            predicted = np.array(agent.predict(self.val[0].copy()))
+            results.append(np.corrcoef(predicted, self.val[1])[0, 1])
+            pbar.update(self.batch)
+        pbar.close()
+        return np.array(results)
+    
+
