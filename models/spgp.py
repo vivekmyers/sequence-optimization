011e1a0d8cad0923365445e1b48d3a76aa12a77d
Vivek Myers <vmyers@stanford.edu>
2019-07-25 19:22:37 -0400

    SPGP

--- models/spgp.py
+++ models/spgp.py
@@ -4,7 +4,7 @@ import os, sys
 import torch
 from torch import nn
 import torch.functional as F
-from torch.distributions import Normal
+from torch.distributions import *
 from models.autoencoder import Autoencoder
 from scipy.spatial.distance import pdist, cdist, squareform
 
@@ -18,35 +18,41 @@ class SparseGaussianProcess:
 
     def _induce(self, X, M, Y, X_pred):
         n = len(X)
-        T = lambda t: torch.tensor(t, requires_grad=True).to(self.embed.device).float()
-        X, Y = map(T, [X, Y])
-        X_bar = torch.empty(M, X.shape[1])
-        for i, t in enumerate(sample(X, M)):
+        T = lambda t: torch.tensor(t, requires_grad=True, device=self.embed.device, dtype=torch.double)
+        X, Y, X_pred = map(T, [X, Y, X_pred])
+        X_bar = torch.empty(M, X.shape[1], device=self.embed.device, requires_grad=True, dtype=torch.double)
+        for i, t in enumerate(sample(list(X), M)):
             X_bar.data[i, :] = t
-        c, b, sig = T(1.), T([1.] * dim), T(1.)
-        K = lambda x, y: c * torch.exp(-1 / 2 * torch.sum(b * (x - y) ** 2)
-
-        K_MN = T([[K(a, b) for a in X] for b in X_bar])
-        K_NM = K_MN.permute(0, 1)
-        K_M = T([[K(a, b) for a in X_bar] for b in X_bar])
-        lam = torch.diag(T([K(x, x) - torch.squeeze(K_NM[i].view(1, n) @ \
-                    torch.inverse(K_M) @ K_NM[i].view(n, 1)) for x, i in enumerate(X)]))
-        I = torch.eye(n) * sig
-        sigma = K_NM @ torch.inverse(K_M) @ K_MN + lam + I
-        prior = T([self.mu] * n)
-        loss = -Normal(prior, sigma).log_prob(Y)
-        opt = torch.optim.Adam([X_bar, c, b, sig], lr=self.alpha)
+        c, b, sig = T(1.), T([1.] * X.size(1)), T(1.)
+        K = lambda x, y: c * torch.exp(-1 / 2 * torch.sum(b * (x - y) ** 2))
+        K_vec = lambda x, y: c * torch.exp(-1 / 2 * 
+                    torch.sum(b[None, None, :] * (x[None, :, :] - y[:, None, :]) ** 2, 
+                        dim=2)).transpose(0, 1)
+        I = torch.eye(n).to(self.embed.device).double()
+        opt = torch.optim.Adam([X_bar, c, b, sig], lr=self.zeta)
 
         for itr in range(self.itr):
-            opt.zero_grad()
-            loss.backward(retain_graph=itr < self.itr - 1)
-            opt.step()
+            K_MN = K_vec(X_bar, X)
+            K_NM = K_MN.transpose(0, 1)
+            K_M = K_vec(X_bar, X_bar) + self.eps * torch.eye(M).to(self.embed.device).double()
+            lam = torch.diag(torch.stack([K(x, x) for x in X]) - torch.diag(K_NM @ torch.inverse(K_M) @ K_MN))
+            sigma = K_NM @ torch.inverse(K_M) @ K_MN + lam + I * sig.exp().add(1).log()
+            prior = T([self.mu] * n)
+            try:
+                loss = -MultivariateNormal(prior, sigma).log_prob(Y).sum()
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+            except RuntimeError:
+                break
 
-        Q_M = K_M + K_MN @ torch.inverse(lam + I) @ K_NM
-        K_star = T([[K(x, x_bar) for x_bar in X_bar] for x in X_pred])
-        mu = torch.squeeze(self.mu + K_star @ torch.inverse(Q_M) @ K_MN @ torch.inverse(lam + I) @ (Y[:, None] - self.mu), dim=1)
-        sig_sq = T([K(x, x) for x in X_pred]) - K_star @ (torch.inverse(K_M) - torch.inverse(Q_M)) @ K_star.permute(1, 0) + sig
-        return mu, sig_sq
+        Q_M = K_M + K_MN @ torch.inverse(lam + I * sig.exp().add(1).log()) @ K_NM
+        K_star = K_vec(X_pred, X_bar)
+        mu = torch.squeeze(self.mu + K_star @ torch.inverse(Q_M) @ K_MN @ \
+                torch.inverse(lam + I * sig.exp().add(1).log()) @ (Y[:, None] - self.mu), dim=1)
+        sig_sq = T([K(x, x) for x in X_pred]) - K_star @ (torch.inverse(K_M) - torch.inverse(Q_M)) \
+                @ K_star.permute(1, 0) + sig.exp().add(1).log()
+        return mu, torch.diag(sig_sq)
 
     def interpolate(self, x, prior={}):
         '''Given observed points in (self.X, self.Y) and points (X, Y) in prior.items(),
@@ -59,15 +65,16 @@ class SparseGaussianProcess:
             X.append(a)
             Y.append(b)
         if len(X) == 0 or len(x) == 0:
-            return np.full([len(x)], self.mu)
-        mu, sig_sq = self._induce(X, min(self.M, len(X)), Y, x)
+            return tuple(np.full([2, len(x)], self.mu))
+        mu, sig_sq = self._induce(self.embed(X), min(self.M, len(X)), Y, self.embed(x))
         return mu.detach().cpu().numpy(), torch.sqrt(sig_sq).detach().cpu().numpy()
 
 
     def __init__(self, encoder, dim, shape, beta=0., alpha=5e-4, 
-                    lam=1e-3, mu=0.5, itr=1000, M=100):
+                    zeta=1e-2, lam=1e-3, mu=0.5, itr=200, M=1000, eps=1e-2):
         '''encoder: convert sequences to one-hot arrays.
-        alpha: embedding and induced point descent learning rate.
+        alpha: embedding learning rate.
+        zeta: induced point ascent learning rate
         shape: sequence shape (len, channels).
         beta: embedding score weighting.
         dim: embedding dimensionality.
@@ -75,16 +82,16 @@ class SparseGaussianProcess:
         mu: GP prior mean.
         M: max number of induced points.
         itr: gradient ascent iterations for induced pseudo-inputs.
+        eps: numerical stability
         '''
         super().__init__()
         self.X, self.Y = (), ()
         self.embed = Autoencoder(encoder, dim=dim, alpha=alpha, shape=shape, lam=lam, beta=beta)
         self.mu = mu
-        self.sigma = sigma
         self.dim = dim
-        self.tau = tau
-        self.eps = eps
         self.alpha = alpha
-        self.itr = 1000
+        self.itr = itr
+        self.eps = eps
         self.M = M
+        self.zeta = zeta
 
