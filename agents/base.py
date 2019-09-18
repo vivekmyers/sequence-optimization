a855dfb7cfa23f195f205f48873f8856ad419a58
Vivek Myers <vivek.myers@gmail.com>
2019-09-18 06:05:44 -0400

    Memory-Efficient Caching

--- agents/base.py
+++ agents/base.py
@@ -10,7 +10,7 @@ class BaseAgent:
         self.seen = {}
         self.prior = prior
         self.batch = batch
-        self.encode = self._memoize(encode)
+        self.encode = encode
         self.shape = shape
     
     def act(self, data):
@@ -25,13 +25,3 @@ class BaseAgent:
         '''Predict sequence scores.'''
         return [choice(list(self.seen.values())) for _ in seqs]
 
-    def _memoize(self, f):
-        '''Memoize pure function of one parameter.'''
-        cache = {}
-        def g(x):
-            if x in cache:
-                return cache[x]
-            else:
-                cache[x] = f(x)
-                return cache[x]
-        return g
